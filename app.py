922d07d0d7b73c3df617f62db8dbbbed3cfca631
hanaasous4 <115111817+hanaasous4@users.noreply.github.com>
2022-10-05 22:55:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+# Defining the API route
 @app.route('/')
 def num_of_occurrence():
     input = request.args.get("arg", default="", type=str)
+    # Initializing dictionary (Using Characters in input String as keys and 0 as default
+    # value for all keys)
     occursence = {}.fromkeys([char for char in input],0)
+    # Iterating over characters in input string and update #of_occurrence for the current char in 
+    # the dictionary by increasing the value by one
     for char in input:
         occursence[char] += 1
     return occursence
