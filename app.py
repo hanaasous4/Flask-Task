13618df749f4058ed229ffbf97cbdf2ee33f56a1
hanaasous4 <115111817+hanaasous4@users.noreply.github.com>
2022-10-10 13:18:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Defining the API route
-@app.route('/')
+@app.route('/occurrence/api' , methods = ['GET'])
 def num_of_occurrence():
     input = request.args.get("arg", default="", type=str)
-    # Check if the input is empty string
-    if len(input) == 0:
-        return "Your input is empty, please provide a String"
+    
     # Initializing dictionary (Using Characters in input String as keys and 0 as default
     # value for all keys)
-    occurence = {}.fromkeys([char for char in input],0)
+    occurrence = {}.fromkeys([char for char in input],0)
+    
+    # Check if the input is empty string
+    if len(occurrence) == 0:
+        return jsonify ({"message": "Your input is empty, please provide a String"})
+
     # Iterating over characters in input string and update #of_occurrence for the current char in 
     # the dictionary by increasing the value by one
     for char in input:
-        occurence[char] += 1
-    return occurence
+        occurrence[char] += 1
+    return jsonify({"Characters Occurrences": occurrence})
 
 if __name__ == "__main__":
     app.run(port=5000, host= '0.0.0.0')
